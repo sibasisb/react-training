cef2a819738bf7e69411a2d9a3f673875e8cae88
sibasisb <sibasisbhattacharjee@gmail.com>
2021-03-29 19:27:57 +0530

    adding product model

---
+++ backend/models/product.js
@@ -0,0 +1,48 @@
+let products=[]
+
+module.exports=class Product {
+    constructor(id,title,description,price){
+        this.id=id
+        this.title=title
+        this.description=description
+
+    }
+
+    save(){
+        if(!this.id){
+            let max=-1
+            products.forEach(prod=>max=max>Number(prod.id)?max:prod.id)
+            let uid=max+1
+            this.id=uid.toString()
+            products.push(this)
+            return true
+        }
+        const product=products.find(prod=>prod.id===this.id)
+        if(!product)
+            return false
+        products=products.map(prod=>{
+            return (prod.id===this.id)?
+            {...this}:
+            prod
+        })
+        return true
+    }
+
+    static getAll(){
+        return products
+    }
+
+    static findProductById(productId){
+        let product=products.find(prod=>prod.id===productId)
+        return product
+    }
+
+    static deleteProduct(productId){
+        newProducts=products.filter(prod=>prod.id===productId)
+        if(newProducts.length===products.length)
+            return false
+        products=newProducts
+        return true
+    }
+
+}
